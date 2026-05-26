e86ec13ea2ee012e4e6c6ef975283809fbcd0adb
nolel1898-hue <nolel1898@gmail.com>
2026-05-26 16:13:22 +0300

    Update creater.py

--- creater.py
+++ creater.py
@@ -151,4 +151,4 @@ class ContinuousGenerator:
 
 if __name__ == "__main__":
     generator = ContinuousGenerator()
-    generator.run_single_cycle(count=100)
+    generator.run_single_cycle(count=1500)
